86c66cc44911e2d9f70a36c41ca044b114bc3ce0
Rade58 <bajic.rade2@gmail.com>
2022-12-23 13:02:13 +0100

    👾

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 const http = require("http");
 
+const server = http.createServer(async (req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    res.statusCode = 200;
+
+    res.end();
+  }
+});
+
+server.listen(3001, () => {
+  console.log("Server on http://localhost:3001");
+});
